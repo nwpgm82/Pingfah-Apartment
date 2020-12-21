60b87845a54709102e28ced1b76e77f7300a3e72
nwpgm82 <66294174+nwpgm82@users.noreply.github.com>
2020-12-21 08:10:52 +0700

    Update selectroom.php

--- pages/employee/daily/selectroom.php
+++ pages/employee/daily/selectroom.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if($_SESSION['level'] == 'admin'){
+    if($_SESSION['level'] == 'employee'){
     include('../../connection.php');
     include('../../../components/sidebarEPY.php');
     $daily_id = $_REQUEST['daily_id'];
