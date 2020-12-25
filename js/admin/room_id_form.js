8b4cdd86ab31916651eced4abbd2fc551ee4ab7b
nwpgm82 <66294174+nwpgm82@users.noreply.github.com>
2020-12-25 17:05:57 +0700

    update roomform

--- js/admin/room_id_form.js
+++ js/admin/room_id_form.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
+    let come = $("#come_date")
+    let title_name = $("#title_name")
+    let firstname = $("#firstname")
+    let lastname = $("#lastname")
+    let nickname = $("#nickname")
+    let id_card = $("#id_card")
+    let birthday = $("#birthday")
+    let age = $("#age")
+    let tel = $("#tel")
+    let email = $("#email")
+    let race = $("#race")
+    let nation = $("#nation")
+    let job = $("#job")
+    let address = $("#address")
+    let id_img = $("#id_img")
+    let home_img = $("#home_img")
     $("#add-btn").click(function () {
         $(".new_customer").remove()
         $("#form-box").show()
     })
-    $("#come_date").dateDropper({
+    come.dateDropper({
         format: "Y-m-d",
         lang: "th",
         theme: "my-style",
@@ -12,4 +28,214 @@ $(document).ready(function () {
         largeOnly: true,
         lock: "to"
     });
+    come.change(function () {
+        if (come.val() == "") {
+            come.css("border-color", "red")
+            come.css("background-image", "url('../../../img/tool/calendar-error.png')")
+            come.addClass("placeholder-error")
+            $("#come_error").html("โปรดระบุวันที่เริ่มเข้าพักของผู้เข้าพัก")
+        } else {
+            come.css("border-color", "")
+            come.css("background-image", "")
+            come.removeClass("placeholder-error")
+            $("#come_error").html("")
+        }
+    })
+    firstname.keyup(function () {
+        if (firstname.val() == "") {
+            firstname.css("border-color", "red")
+            firstname.addClass("placeholder-error")
+            $("#fs_error").html("โปรดระบุชื่อของผู้เข้าพัก")
+        } else {
+            firstname.css("border-color", "")
+            firstname.removeClass("placeholder-error")
+            $("#fs_error").html("")
+        }
+    })
+    lastname.keyup(function () {
+        if (lastname.val() == "") {
+            lastname.css("border-color", "red")
+            lastname.addClass("placeholder-error")
+            $("#ls_error").html("โปรดระบุนามสกุลของผู้เข้าพัก")
+        } else {
+            lastname.css("border-color", "")
+            lastname.removeClass("placeholder-error")
+            $("#ls_error").html("")
+        }
+    })
+    nickname.keyup(function () {
+        if (nickname.val() == "") {
+            nickname.css("border-color", "red")
+            nickname.addClass("placeholder-error")
+            $("#nk_error").html("โปรดระบุชื่อเล่นของผู้เข้าพัก")
+        } else {
+            nickname.css("border-color", "")
+            nickname.removeClass("placeholder-error")
+            $("#nk_error").html("")
+        }
+    })
+    id_card.keyup(function (event) {
+        // if (event.which !== 8 && event.which !== 0 && event.which < 48 || event.which > 57) {
+        //     $(this).val(function (index, value) {
+        //         return value.replace(/\D/g, "");
+        //     });
+        // }
+        if (id_card.val() == "") {
+            id_card.css("border-color", "red")
+            id_card.addClass("placeholder-error")
+            $("#id_error").html("โปรดระบุเลขบัตรประชาชน หรือ Passport No.")
+        } else {
+            id_card.css("border-color", "")
+            id_card.removeClass("placeholder-error")
+            $("#id_error").html("")
+        }
+    });
+    birthday.keyup(function () {
+        if (birthday.val() == "") {
+            birthday.css("border-color", "red")
+            birthday.addClass("placeholder-error")
+            $("#bd_error").html("โปรดระบุวันเกิดของผู้เข้าพัก")
+        } else {
+            birthday.css("border-color", "")
+            birthday.removeClass("placeholder-error")
+            $("#bd_error").html("")
+        }
+    })
+    tel.keyup(function (event) {
+        if (event.which !== 8 && event.which !== 0 && event.which < 48 || event.which > 57) {
+            $(this).val(function (index, value) {
+                return value.replace(/\D/g, "");
+            });
+        }
+        if (tel.val() == "") {
+            tel.css("border-color", "red")
+            tel.addClass("placeholder-error")
+            $("#tel_error").html("โปรดระบุเบอร์โทรศัพท์ของผู้เข้าพัก")
+        } else {
+            tel.css("border-color", "")
+            tel.removeClass("placeholder-error")
+            $("#tel_error").html("")
+        }
+    })
+    email.keyup(function () {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (email.val() == "") {
+            email.css("border-color", "red")
+            email.addClass("placeholder-error")
+            $("#em_error").html("โปรดระบุอีเมล")
+        } else {
+            if (re.test(String(email.val()))) {
+                email.css("border-color", "")
+                email.removeClass("placeholder-error")
+                $("#em_error").html("")
+            } else {
+                email.css("border-color", "red")
+                email.addClass("placeholder-error")
+                $("#em_error").html("รูปแบบไม่ตรงกัน")
+            }
+        }
+    })
+    race.keyup(function () {
+        if (race.val() == "") {
+            race.css("border-color", "red")
+            race.addClass("placeholder-error")
+            $("#rc_error").html("โปรดระบุเชื้อชาติของผู้เข้าพัก")
+        } else {
+            race.css("border-color", "")
+            race.removeClass("placeholder-error")
+            $("#rc_error").html("")
+        }
+    })
+    nation.keyup(function () {
+        if (nation.val() == "") {
+            nation.css("border-color", "red")
+            nation.addClass("placeholder-error")
+            $("#na_error").html("โปรดระบุสัญชาติของผู้เข้าพัก")
+        } else {
+            nation.css("border-color", "")
+            nation.removeClass("placeholder-error")
+            $("#na_error").html("")
+        }
+    })
+    job.keyup(function () {
+        if (job.val() == "") {
+            job.css("border-color", "red")
+            job.addClass("placeholder-error")
+            $("#job_error").html("โปรดระบุอาชีพของผู้เข้าพัก")
+        } else {
+            job.css("border-color", "")
+            job.removeClass("placeholder-error")
+            $("#job_error").html("")
+        }
+    })
+    address.keyup(function () {
+        if (address.val() == "") {
+            address.css("border-color", "red")
+            address.addClass("placeholder-error")
+            $("#ad_error").html("โปรดระบุที่อยู่ของผู้เข้าพัก")
+        } else {
+            address.css("border-color", "")
+            address.removeClass("placeholder-error")
+            $("#ad_error").html("")
+        }
+    })
+    $("button[type=submit]").click(function (event) {
+        var inputs = $("input");
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        inputs.each(function () {
+            if ($(this).val() == "") {
+                if ($(this).attr("id") == "come_date") {
+                    $(this).css("border-color", "red")
+                    $(this).css("background-image", "url('../../../img/tool/calendar-error.png')")
+                    $(this).addClass("placeholder-error")
+                    $("#come_error").html("โปรดระบุวันที่เริ่มเข้าพักของผู้เข้าพัก")
+                } else if ($(this).attr("id") == "id_img") {
+                    $("#id_box").css("border-color", "red")
+                    $("#idimg_error").html("โปรดเพิ่มรูปภาพสำเนาบัตรประชาชน")
+                } else if ($(this).attr("id") == "home_img") {
+                    $("#home_box").css("border-color", "red")
+                    $("#homeimg_error").html("โปรดเพิ่มรูปภาพสำเนาทะเบียนบ้าน")
+                } else {
+                    $(this).css("border-color", "red")
+                    $(this).addClass("placeholder-error")
+                    if ($(this).attr("id") == "firstname") {
+                        $("#fs_error").html("โปรดระบุชื่อของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "lastname") {
+                        $("#ls_error").html("โปรดระบุนามสกุลของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "nickname") {
+                        $("#nk_error").html("โปรดระบุชื่อเล่นของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "id_card") {
+                        $("#id_error").html("โปรดระบุเลขบัตรประชาชน หรือ Passport No.")
+                    } else if ($(this).attr("id") == "birthday") {
+                        $("#bd_error").html("โปรดระบุวันเกิดของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "age") {
+                        $("#ag_error").html("โปรดระบุอายุของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "tel") {
+                        $("#tel_error").html("โปรดระบุเบอร์โทรศัพท์ของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "email") {
+                        $("#em_error").html("โปรดระบุอีเมล")
+                    } else if ($(this).attr("id") == "race") {
+                        $("#rc_error").html("โปรดระบุเชื้อชาติของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "nation") {
+                        $("#na_error").html("โปรดระบุสัญชาติของผู้เข้าพัก")
+                    } else if ($(this).attr("id") == "job") {
+                        $("#job_error").html("โปรดระบุอาชีพของผู้เข้าพัก")
+                    }
+                }
+                event.preventDefault()
+            }
+        })
+        if (email.val() != "") {
+            if (!re.test(String(email.val()))) {
+                $("#em_error").html("รูปแบบไม่ตรงกัน")
+                event.preventDefault()
+            }
+        }
+        if (address.val() == "") {
+            address.css("border-color", "red")
+            address.addClass("placeholder-error")
+            $("#ad_error").html("โปรดระบุที่อยู่ของผู้เข้าพัก")
+            event.preventDefault()
+        }
+    })
 })
--- pages/admin/roomList/room_id.php
+++ pages/admin/roomList/room_id.php
@@ -3,7 +3,8 @@ session_start();
 if($_SESSION["level"] == "admin"){
     include("../../connection.php");
     $room_id = $_REQUEST["ID"];
-    $sql = "SELECT * FROM roommember WHERE member_status = 'เข้าพักอยู่'";
+    $sql = "SELECT * FROM roommember WHERE room_id = '$room_id'";
+
 ?>
 
 <!DOCTYPE html>
@@ -35,13 +36,14 @@ if($_SESSION["level"] == "admin"){
                     </div>
                 </div>
                 <div id="form-box" style="display:none;">
-                    <form action="">
+                    <form action="function/addData.php?ID=<?php echo $room_id; ?>" method="POST" enctype="multipart/form-data">
                         <h3>ห้อง <?php echo $room_id; ?></h3>
                         <div class="hr"></div>
                         <div class="grid-container">
                             <div class="come">
                                 <p>วันที่เริ่มเข้าพัก</p>
-                                <input type="text" id="come_date">
+                                <input type="text" name="come" id="come_date">
+                                <h5 id="come_error" style="color:red;"></h5>
                             </div>
                             <!-- <div class="status">
                                 <p>สถานะการเข้าพัก</p>
@@ -49,7 +51,7 @@ if($_SESSION["level"] == "admin"){
                             </div> -->
                             <div class="title_name">
                                 <p>คำนำหน้าชื่อ</p>
-                                <select name="" id="">
+                                <select name="title_name" id="title_name">
                                     <option value="นาย" selected>นาย</option>
                                     <option value="นาง">นาง</option>
                                     <option value="นางสาว">นางสาว</option>
@@ -57,52 +59,64 @@ if($_SESSION["level"] == "admin"){
                             </div>
                             <div class="firstname">
                                 <p>ชื่อ</p>
-                                <input type="text" placeholder="ชื่อ">
+                                <input type="text" name="firstname" id="firstname" placeholder="ชื่อ">
+                                <h5 id="fs_error" style="color:red;"></h5>
                             </div>
                             <div class="lastname">
                                 <p>นามสกุล</p>
-                                <input type="text" placeholder="นามสกุล">
+                                <input type="text" name="lastname" id="lastname" placeholder="นามสกุล">
+                                <h5 id="ls_error" style="color:red;"></h5>
                             </div>
                             <div class="nickname">
                                 <p>ชื่อเล่น</p>
-                                <input type="text" placeholder="ชื่อเล่น">
+                                <input type="text" name="nickname" id="nickname" placeholder="ชื่อเล่น">
+                                <h5 id="nk_error" style="color:red;"></h5>
                             </div>
                             <div class="id_card">
                                 <p>เลขบัตรประชาชน / Passport No.</p>
-                                <input type="text" placeholder="เลขบัตรประชาชน / Passport No." maxlength="13">
+                                <input type="text" name="id_card" id="id_card" placeholder="เลขบัตรประชาชน / Passport No." maxlength="13">
+                                <h5 id="id_error" style="color:red;"></h5>
                             </div>
                             <div class="birthday">
                                 <p>วัน / เดือน / ปีเกิด</p>
-                                <input type="text">
+                                <input type="text" name="birthday" id="birthday">
+                                <h5 id="bd_error" style="color:red;"></h5>
                             </div>
                             <div class="age">
                                 <p>อายุ</p>
-                                <input type="number" min="23" max="60" oninput="this.value = this.value > 60 ? 60 : Math.abs(this.value)" placeholder="อายุ" readonly>
+                                <input type="number" name="age" id="age" min="23" max="60" oninput="this.value = this.value > 60 ? 60 : Math.abs(this.value)" placeholder="อายุ" readonly>
+                                <h5 id="ag_error" style="color:red;"></h5>
                             </div>
                             <div class="phone">
                                 <p>เบอร์โทรศัพท์</p>
-                                <input type="text" maxlength="10" placeholder="เบอร์โทรศัพท์">
+                                <input type="text" name="tel" id="tel" maxlength="10" placeholder="เบอร์โทรศัพท์">
+                                <h5 id="tel_error" style="color:red;"></h5>
                             </div>
                             <div class="email">
                                 <p>อีเมล</p>
-                                <input type="email" placeholder="อีเมล">
+                                <input type="email" name="email" id="email" placeholder="อีเมล">
+                                <h5 id="em_error" style="color:red;"></h5>
                             </div>
                             <div class="race">
                                 <p>เชื้อชาติ</p>
-                                <input type="text" placeholder="เชื้อชาติ">
+                                <input type="text" name="race" id="race" placeholder="เชื้อชาติ" value="ไทย">
+                                <h5 id="rc_error" style="color:red;"></h5>
                             </div>
                             <div class="nationality">
                                 <p>สัญชาติ</p>
-                                <input type="text" placeholder="สัญชาติ">
+                                <input type="text" name="nation" id="nation" placeholder="สัญชาติ" value="ไทย">
+                                <h5 id="na_error" style="color:red;"></h5>
                             </div>
                             <div class="job">
                                 <p>อาชีพ</p>
-                                <input type="text" placeholder="อาชีพ">
+                                <input type="text" name="job" id="job" placeholder="อาชีพ">
+                                <h5 id="job_error" style="color:red;"></h5>
                             </div>
                         </div>
-                        <div style="padding-top:16px;">
+                        <div style="padding-top:16px;height:146px;">
                             <p>ที่อยู่</p>
-                            <textarea name="" id="" placeholder="ที่อยู่"></textarea>
+                            <textarea name="address" id="address" placeholder="ที่อยู่"></textarea>
+                            <h5 id="ad_error" style="color:red;"></h5>
                         </div>
                         <div style="padding-top:32px;">
                             <h3>สำเนาเอกสาร</h3>
@@ -110,23 +124,25 @@ if($_SESSION["level"] == "admin"){
                             <div class="img-grid">
                                 <div>
                                     <p>สำเนาบัตรประชาชน</p>
-                                    <div class="img-box">
+                                    <div class="img-box" id="id_box">
 
                                     </div>
-                                    <input type="file" name="" id="">
+                                    <h5 id="idimg_error" style="color:red;"></h5>
+                                    <input type="file" name="id_img" id="id_img">
                                 </div>
                                 <div>
                                     <p>สำเนาทะเบียนบ้าน</p>
-                                    <div class="img-box">
+                                    <div class="img-box" id="home_box">
 
                                     </div>
-                                    <input type="file" name="" id="">
+                                    <h5 id="homeimg_error" style="color:red;"></h5>
+                                    <input type="file" name="home_img" id="home_img">
                                 </div>
                             </div>
                         </div>
                         <div class="hr"></div>
                         <div style="padding-top:32px;display:flex;justify-content:center;align-items:center;">
-                            <button>เพิ่มผู้พัก</button>
+                            <button type="submit">เพิ่มผู้พัก</button>
                         </div>
                     </form>
                 </div>
