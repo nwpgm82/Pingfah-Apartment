c1bac6f6a7229327e16ee3feb5bf780f00db5c06
nwpgm82 <66294174+nwpgm82@users.noreply.github.com>
2021-03-06 00:25:06 +0700

    update room_id
    - fixed id_card & Passport No. (7, 8, 9, 13)

--- pages/manage/roomList/addmember.php
+++ pages/manage/roomList/addmember.php
@@ -7,7 +7,7 @@ if($_SESSION["level"] == "admin"){
         function DateThai($strDate){
             $strYear = date("Y",strtotime($strDate));
             $strMonth= date("n",strtotime($strDate));
-            $strDay= date("j",strtotime($strDate));
+            $strDay= date("d",strtotime($strDate));
             $strMonthCut = Array("","มกราคม", "กุมภาพันธ์", "มีนาคม","เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม","สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม");
             $strMonthThai=$strMonthCut[$strMonth];
             return "$strDay $strMonthThai $strYear";
